84a564dc8d50cf776d4cc42fbf227071797a9179
dragonWoo <mire2566@naver.com>
2021-04-14 10:35:17 +0900

    git commit first

--- login/src/main/java/com/sign/login/domain/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sign.login.domain.repository;
-
-import com.sign.login.domain.entity.MemberEntity;
-
-import java.util.Optional;
-
-public interface MemberRepository {
-    Optional<MemberEntity> findByEmail(String userEmail);
-}
