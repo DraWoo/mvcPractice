c4cff44d0863af3069ec76ea1fae14f6fe3bbbe7
dragonWoo <mire2566@naver.com>
2021-04-14 11:40:35 +0900

    git commit first

---
+++ src/main/java/com/sign/login/dto/MemberDto.java
@@ -0,0 +1,34 @@
+package com.sign.login.dto;
+
+
+import com.sign.login.domain.entity.MemberEntity;
+import lombok.*;
+import org.springframework.security.access.event.PublicInvocationEvent;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class MemberDto {
+    private Long id;
+    private String email;
+    private String password;
+    private LocalDateTime createDate;
+    private LocalDateTime modifidDate;
+
+    public MemberEntity toEntity(){
+        return MemberEntity.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .build();
+    }
+    @Builder
+    public MemberDto(Long id, String email, String password){
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+}
